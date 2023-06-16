98d3f3633575ec96f82061fdd6c0ec9d132b726b
cooper <lyt3131@163.com>
2023-06-16 18:46:12 +0800

    jre 11

--- canal-server/docker_build.sh
+++ canal-server/docker_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker buildx build . --platform linux/amd64,linux/arm64 --push -t coopersoft/canal-server:stanlone_10.9.7_1.1.7
+docker buildx build . --platform linux/amd64,linux/arm64 --push -t coopersoft/canal-server:standalone_10.9.7_1.1.7
