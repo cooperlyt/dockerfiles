a48124698479bfe72a4f2dc598761b77042c785e
cooper <lyt3131@163.com>
2023-08-13 06:15:00 +0800

    weedfs image

--- weedfs-gateway/lua/media.lua
+++ weedfs-gateway/lua/media.lua
@@ -1,257 +1,283 @@
-function table.contains(table, element)
-  for _, value in pairs(table) do
-      if value == element then
-          return true
-      end
-  end
-  return false
-end
+-- function table.contains(table, element)
+--   for _, value in pairs(table) do
+--       if value == element then
+--           return true
+--       end
+--   end
+--   return false
+-- end
 
-function file_exists(name)
-  local f=io.open(name,"r")
-  if f~=nil then io.close(f) return true else return false end
-end
+-- function file_exists(name)
+--   local f=io.open(name,"r")
+--   if f~=nil then io.close(f) return true else return false end
+-- end
 
-function exit_with_code(code)
---    ngx.say(code) 
-  ngx.exit(code)
-  return
-end
+-- function exit_with_code(code)
+-- --    ngx.say(code) 
+--   ngx.exit(code)
+--   return
+-- end
 
-function req_orig_file(file_url)
-  ngx.log(ngx.INFO,"req_orig_file:",file_url)
+-- function req_orig_file(file_url)
+--   ngx.log(ngx.INFO,"req_orig_file:",file_url)
   
-  local http = require"resty.http"
-  local hc = http.new()
-  local res, err = hc:request_uri(file_url)
+--   local http = require"resty.http"
+--   local hc = http.new()
+--   local res, err = hc:request_uri(file_url)
 
-  ngx.log(ngx.INFO,"req_orig_file1:",res.status)
+--   ngx.log(ngx.INFO,"req_orig_file1:",res.status)
 
-  if res.status >= 300 and res.status < 400 then
-    -- res.headers["Location"]
-    file_url = string.match(res.body,'"(.+)"')
-    ngx.log(ngx.INFO,"refer",file_url)
-    res, err = hc:request_uri(file_url)
-    ngx.log(ngx.INFO,"req_orig_file2:",res.status)
-  end
-
-
-
-  if res.status ~= 200 then
-    ngx.log(ngx.ERR,"req_orig_file error:",err)
-    return exit_with_code(404)
-  else
-    if res.body == nil then
-      ngx.log(ngx.ERR,"req_orig_file error:body is nil")
-      return exit_with_code(404)
-    else
-      if (res.body..'a') == 'a' then
-          ngx.log(ngx.ERR,"req_orig_file error:body is empty")
-          return exit_with_code(404)
-        else
-          ngx.say(res.body)
-          ngx.flush(true)
-          exit_with_code(200)
-          return
-      end
-    end
-  end
-end
+--   if res.status >= 300 and res.status < 400 then
+--     -- res.headers["Location"]
+--     file_url = string.match(res.body,'"(.+)"')
+--     ngx.log(ngx.INFO,"refer",file_url)
+--     res, err = hc:request_uri(file_url)
+--     ngx.log(ngx.INFO,"req_orig_file2:",res.status)
+--   end
 
 
-function save_orig_file(file_url,local_file_folder,local_file_path)
-  local http = require "resty.http"
-  local hc = http.new()
-  local res, err = hc:request_uri(file_url)
 
-  if res.status == 301 or res.status == 302 then
-     file_url = string.match(res.body,'"(.+)"')
-     res, err = hc:request_uri(file_url)
-  end
-
-  if res.status ~= 200 then
-      return exit_with_code(404)
-  else
-      if res.body == nil then
-          return exit_with_code(404)
-      else
-          if (res.body..'a') == 'a' then
-              return exit_with_code(404)
-          else
-              local mkdir_command ="mkdir -p "..local_file_folder.." >/dev/null 2>&1 "
-              os.execute(mkdir_command)
-              file = io.open(local_file_path, "w")
-              if (file) then
-                  file:write(res.body)
-                  file:close()
-              else
-                  return exit_with_code(500)
-              end
-          end
-      end
-  end
-end
-
-function req_volume_server()
--- TODO,get from weedfs,curl http://localhost:9333/dir/lookup?volumeId=3
-end
+--   if res.status ~= 200 then
+--     ngx.log(ngx.ERR,"req_orig_file error:",err)
+--     return exit_with_code(404)
+--   else
+--     if res.body == nil then
+--       ngx.log(ngx.ERR,"req_orig_file error:body is nil")
+--       return exit_with_code(404)
+--     else
+--       if (res.body..'a') == 'a' then
+--           ngx.log(ngx.ERR,"req_orig_file error:body is empty")
+--           return exit_with_code(404)
+--         else
+--           ngx.say(res.body)
+--           ngx.flush(true)
+--           exit_with_code(200)
+--           return
+--       end
+--     end
+--   end
+-- end
 
 
-function process_img(file_volumn,file_id,file_size,file_url)
-  local image_sizes = { "100x100", "80x80", "800x600", "40x40" ,"480x320","360x200","320x210","640x420","160x160","800x400","200x200"}
-  local scale_image_sizes = { "100x100s", "80x80s", "800x600s", "40x40s" ,"480x320s","360x200s","320x210s","640x420s","160x160s","800x400s","200x200s"}
-  local local_file_root =  ngx.var.local_img_fs_root .."images/"
-  local local_file_in_folder = local_file_root .."orig/".. file_volumn .."/"
-  local local_file_in_path = local_file_in_folder.. file_id ..".jpg"
+-- function save_orig_file(file_url,local_file_folder,local_file_path)
+--   local http = require "resty.http"
+--   local hc = http.new()
+--   local res, err = hc:request_uri(file_url)
 
-  local local_file_out_folder = local_file_root.. file_size .."/" .. file_volumn .."/"
-  local local_file_out_path = local_file_out_folder.. file_id ..".jpg"
-  local local_file_out_rel_path = "/images/".. file_size .."/" .. file_volumn .."/".. file_id ..".jpg"
+--   if res.status == 301 or res.status == 302 then
+--      file_url = string.match(res.body,'"(.+)"')
+--      res, err = hc:request_uri(file_url)
+--   end
 
-  local mkdir_command ="mkdir -p "..local_file_out_folder.." >/dev/null 2>&1 "
-  local convert_command
+--   if res.status ~= 200 then
+--       return exit_with_code(404)
+--   else
+--       if res.body == nil then
+--           return exit_with_code(404)
+--       else
+--           if (res.body..'a') == 'a' then
+--               return exit_with_code(404)
+--           else
+--               local mkdir_command ="mkdir -p "..local_file_folder.." >/dev/null 2>&1 "
+--               os.execute(mkdir_command)
+--               file = io.open(local_file_path, "w")
+--               if (file) then
+--                   file:write(res.body)
+--                   file:close()
+--               else
+--                   return exit_with_code(500)
+--               end
+--           end
+--       end
+--   end
+-- end
 
-      --return if has a local copy
-  if(file_exists(local_file_out_path))then
-      local file = io.open(local_file_out_path, "r")
-      if (file) then
-          local content= file:read("*a")
-          file:close()
-          ngx.say(content)
-          ngx.flush(true)
-          return
-      end
-  end
-
-  --get original file
-  if file_size == "orig" then
-      return req_orig_file(file_url)
-  end
-
-  if table.contains(scale_image_sizes, file_size) then
-      file_size=string.sub(file_size, 1, -2)
-      convert_command = "gm convert " .. local_file_in_path .. " -resize '" .. file_size .. "'  -quality 90  "  .. local_file_out_path .. ">/dev/null 2>&1 "
-  elseif (table.contains(image_sizes, file_size)) then
-      convert_command = "gm convert " .. local_file_in_path .. " -thumbnail " .. file_size .. "^  -quality 90  -gravity center -extent " .. file_size .. " " .. local_file_out_path .. ">/dev/null 2>&1 "
-  else
-      return exit_with_code(404)
-  end
-  --ngx.say('enter')
-  if(not file_exists(local_file_in_path))then
-      save_orig_file(file_url,local_file_in_folder,local_file_in_path)
-  end
-
-  os.execute(mkdir_command)
-  os.execute(convert_command)
-
-  if(file_exists(local_file_out_path))then
-      local file = io.open(local_file_out_path, "r")
-      if (file) then
-          local content= file:read("*a")
-          file:close()
-          ngx.say(content)
-          ngx.flush(true)
-      else
-          return exit_with_code(500)
-      end
-  end
-
-end
-
-function process_audio(file_volumn,file_id,file_size,file_url)
-
-  local audio_sizes = { "mp3" }
-  local local_file_root = ngx.var.local_audio_fs_root .."audios/"
-  local local_file_in_folder = local_file_root .."orig/".. file_volumn .."/"
-  local local_file_in_path = local_file_in_folder.. file_id ..".mp3"
-
-  local local_file_out_folder = local_file_root.. file_size .."/" .. file_volumn .."/"
-  local local_file_out_path = local_file_out_folder.. file_id ..".mp3"
-  local local_file_out_rel_path = "/audios/".. file_size .."/" .. file_volumn .."/".. file_id ..".mp3"
-
-  if(file_exists(local_file_out_path))then
-    local file = io.open(local_file_out_path, "r")
-      if (file) then
-        local content= file:read("*a")
-        file:close()
-        ngx.say(content)
-        ngx.flush(true)
-        return
-      end
-  end
-
-  --get original file
-  if file_size == "orig" then
-    return req_orig_file(file_url)
-  end
-
-  if table.contains(audio_sizes, file_size) then
-      if(not file_exists(local_file_in_path))then
-          save_orig_file(file_url,local_file_in_folder,local_file_in_path)
-      end
-
-      if(file_exists(local_file_in_path))then
-          local mkdir_command ="mkdir -p "..local_file_out_folder.." >/dev/null 2>&1 "
-          local convert_command = "ffmpeg -i " .. local_file_in_path .. " -ab 64 " .. local_file_out_path .. " >/dev/null 2>&1 "
-          os.execute(mkdir_command)
-          os.execute(convert_command)
-          if(file_exists(local_file_out_path))then
-              local file = io.open(local_file_out_path, "r")
-              if (file) then
-                  local content= file:read("*a")
-                  file:close()
-                  ngx.say(content)
-                  ngx.flush(true)
-              else
-                  return exit_with_code(500)
-              end
-          end
-      end
-  else
-      return exit_with_code(404)
-  end
-end
+-- function req_volume_server()
+-- -- TODO,get from weedfs,curl http://localhost:9333/dir/lookup?volumeId=3
+-- end
 
 
+-- function process_img(file_volumn,file_id,file_size,file_url)
+--   local image_sizes = { "100x100", "80x80", "800x600", "40x40" ,"480x320","360x200","320x210","640x420","160x160","800x400","200x200"}
+--   local scale_image_sizes = { "100x100s", "80x80s", "800x600s", "40x40s" ,"480x320s","360x200s","320x210s","640x420s","160x160s","800x400s","200x200s"}
+--   local local_file_root =  ngx.var.local_img_fs_root .."images/"
+--   local local_file_in_folder = local_file_root .."orig/".. file_volumn .."/"
+--   local local_file_in_path = local_file_in_folder.. file_id ..".jpg"
+
+--   local local_file_out_folder = local_file_root.. file_size .."/" .. file_volumn .."/"
+--   local local_file_out_path = local_file_out_folder.. file_id ..".jpg"
+--   local local_file_out_rel_path = "/images/".. file_size .."/" .. file_volumn .."/".. file_id ..".jpg"
+
+--   local mkdir_command ="mkdir -p "..local_file_out_folder.." >/dev/null 2>&1 "
+--   local convert_command
+
+--       --return if has a local copy
+--   if(file_exists(local_file_out_path))then
+--       local file = io.open(local_file_out_path, "r")
+--       if (file) then
+--           local content= file:read("*a")
+--           file:close()
+--           ngx.say(content)
+--           ngx.flush(true)
+--           return
+--       end
+--   end
+
+--   --get original file
+--   if file_size == "orig" then
+--       return req_orig_file(file_url)
+--   end
+
+--   if table.contains(scale_image_sizes, file_size) then
+--       file_size=string.sub(file_size, 1, -2)
+--       convert_command = "gm convert " .. local_file_in_path .. " -resize '" .. file_size .. "'  -quality 90  "  .. local_file_out_path .. ">/dev/null 2>&1 "
+--   elseif (table.contains(image_sizes, file_size)) then
+--       convert_command = "gm convert " .. local_file_in_path .. " -thumbnail " .. file_size .. "^  -quality 90  -gravity center -extent " .. file_size .. " " .. local_file_out_path .. ">/dev/null 2>&1 "
+--   else
+--       return exit_with_code(404)
+--   end
+--   --ngx.say('enter')
+--   if(not file_exists(local_file_in_path))then
+--       save_orig_file(file_url,local_file_in_folder,local_file_in_path)
+--   end
+
+--   os.execute(mkdir_command)
+--   os.execute(convert_command)
+
+--   if(file_exists(local_file_out_path))then
+--       local file = io.open(local_file_out_path, "r")
+--       if (file) then
+--           local content= file:read("*a")
+--           file:close()
+--           ngx.say(content)
+--           ngx.flush(true)
+--       else
+--           return exit_with_code(500)
+--       end
+--   end
+
+-- end
+
+-- function process_audio(file_volumn,file_id,file_size,file_url)
+
+--   local audio_sizes = { "mp3" }
+--   local local_file_root = ngx.var.local_audio_fs_root .."audios/"
+--   local local_file_in_folder = local_file_root .."orig/".. file_volumn .."/"
+--   local local_file_in_path = local_file_in_folder.. file_id ..".mp3"
+
+--   local local_file_out_folder = local_file_root.. file_size .."/" .. file_volumn .."/"
+--   local local_file_out_path = local_file_out_folder.. file_id ..".mp3"
+--   local local_file_out_rel_path = "/audios/".. file_size .."/" .. file_volumn .."/".. file_id ..".mp3"
+
+--   if(file_exists(local_file_out_path))then
+--     local file = io.open(local_file_out_path, "r")
+--       if (file) then
+--         local content= file:read("*a")
+--         file:close()
+--         ngx.say(content)
+--         ngx.flush(true)
+--         return
+--       end
+--   end
+
+--   --get original file
+--   if file_size == "orig" then
+--     return req_orig_file(file_url)
+--   end
+
+--   if table.contains(audio_sizes, file_size) then
+--       if(not file_exists(local_file_in_path))then
+--           save_orig_file(file_url,local_file_in_folder,local_file_in_path)
+--       end
+
+--       if(file_exists(local_file_in_path))then
+--           local mkdir_command ="mkdir -p "..local_file_out_folder.." >/dev/null 2>&1 "
+--           local convert_command = "ffmpeg -i " .. local_file_in_path .. " -ab 64 " .. local_file_out_path .. " >/dev/null 2>&1 "
+--           os.execute(mkdir_command)
+--           os.execute(convert_command)
+--           if(file_exists(local_file_out_path))then
+--               local file = io.open(local_file_out_path, "r")
+--               if (file) then
+--                   local content= file:read("*a")
+--                   file:close()
+--                   ngx.say(content)
+--                   ngx.flush(true)
+--               else
+--                   return exit_with_code(500)
+--               end
+--           end
+--       end
+--   else
+--       return exit_with_code(404)
+--   end
+-- end
+
+
+-- local file_volumn = ngx.var.arg_volumn
+-- local file_id = ngx.var.arg_id
+-- local file_url = ngx.var.weed_img_root_url .. file_volumn .. "," .. file_id
+-- local process_type = ngx.var.arg_type or "na"
+-- local file_size = ngx.var.arg_size or "na"
+
+-- if ngx.var.arg_size == nil or ngx.var.arg_volumn == nil or ngx.var.arg_id == nil then
+--   ngx.log(ngx.ERR, "missing params - size:" , ngx.var.arg_size , " volumn:" , ngx.var.arg_volumn , " id:" , ngx.var.arg_id)
+--   return exit_with_code(400)
+-- end
+
+-- if(process_type == "img") then
+--   ngx.log(ngx.INFO, "process image - ", "volumn:" , file_volumn , " id:" , file_id , " size:" , file_size , " url:" , file_url)
+--   process_img(file_volumn,file_id,file_size,file_url)
+-- elseif(process_type == "audio")then
+--   ngx.log(ngx.INFO, "process audio - " , "volumn:" , file_volumn , " id:" , file_id , " size:" , file_size , " url:" , file_url)
+--   process_audio(file_volumn,file_id,file_size,file_url)
+-- end
+
+-------------------------------
 local file_volumn = ngx.var.arg_volumn
 local file_id = ngx.var.arg_id
-local file_url = ngx.var.weed_img_root_url .. file_volumn .. "," .. file_id
-local process_type = ngx.var.arg_type or "na"
-local file_size = ngx.var.arg_size or "na"
+local f_id = file_volumn .. "," .. file_id
 
-if ngx.var.arg_size == nil or ngx.var.arg_volumn == nil or ngx.var.arg_id == nil then
-  ngx.log(ngx.ERR, "missing params - size:" , ngx.var.arg_size , " volumn:" , ngx.var.arg_volumn , " id:" , ngx.var.arg_id)
-  return exit_with_code(400)
-end
-
-if(process_type == "img") then
-  ngx.log(ngx.INFO, "process image - ", "volumn:" , file_volumn , " id:" , file_id , " size:" , file_size , " url:" , file_url)
-  process_img(file_volumn,file_id,file_size,file_url)
-elseif(process_type == "audio")then
-  ngx.log(ngx.INFO, "process audio - " , "volumn:" , file_volumn , " id:" , file_id , " size:" , file_size , " url:" , file_url)
-  process_audio(file_volumn,file_id,file_size,file_url)
-end
-
-
-local magick = require('resty.magick.init')
-
+local seaweedfs = require "resty.seaweedfs"
 local options = {
-  width = ngx.var.image_width,
-  height = ngx.var.image_height,
-  quality = ngx.var.image_quality,
-  format = ngx.var.image_extension,
+  master_url = ngx.var.weed_img_root_url
 }
-local magic = magick:new(options)
-local ok, err = magic:load_file(local_file_in_path)
-if not ok then
-  ngx.log(ngx.ERR, err)
-  return
-end
-local webp_options = { quality = 75, lossless = "0" }
-ok, err = magic:set_format('webp', webp_options)
-if not ok then
+local fs = seaweedfs:new(options)
+local res, err = fs:get(f_id)
+
+if res.status == 200 then
+  local magick = require('resty.magick.init')
+
+  local options = {
+    width = 800,
+    height = 600,
+    quality = 75,
+    format = 'webp',
+  }
+  local magic = magick:new(options)
+  local ok, err = magic:load_image(res.body)
+  if not ok then
+    ngx.status = 500
     ngx.log(ngx.ERR, err)
-    return
+    ngx.say(err)
+    return ngx.exit(500)
+  end
+  magick:resize()
+  local webp_options = { quality = 75, lossless = "0" }
+  ok, err = magic:set_format('webp', webp_options)
+  if not ok then
+    ngx.status = 500
+    ngx.log(ngx.ERR, err)
+    ngx.say(err)
+    return ngx.exit(500)
+  else
+    local blob = magic:get_blob()
+    ngx.header[key] = res.headers["content-type"]
+    ngx.say(blob)
+    return ngx.exit(200)
+  end
+else
+  ngx.status = res.status
+  ngx.say(err)
+  return ngx.exit(res.status)
 end
-local blob = magic:get_blob()
-ngx.print(blob)
+
