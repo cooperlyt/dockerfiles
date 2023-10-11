a97fee3deadfdcdf52a10dd8700843b4ffb50ec6
cooper <lyt3131@163.com>
2023-10-11 17:53:06 +0800

    version 1.1.7

--- canal-server/docker_build.sh
+++ canal-server/docker_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker buildx build . --platform linux/amd64,linux/arm64 --push -t coopersoft/canal-server:latest
+docker buildx build . --platform linux/amd64,linux/arm64 --push -t coopersoft/canal-server:1.1.7
