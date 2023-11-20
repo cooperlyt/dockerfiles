adb6f7ef67fb0f0861c136d98ebdee0cfe4ce847
cooper <lyt3131@163.com>
2023-11-21 04:19:28 +0800

    hmplat

--- elasticsearch/docker_build.sh
+++ elasticsearch/docker_build.sh
@@ -1,3 +1,3 @@
 # docker build -t coopersoft/elasticsearch:8.9.0 .
 
-docker buildx build . --platform linux/amd64,linux/arm64 --push -t coopersoft/elasticsearch:8.9.0 
+docker buildx build . --platform linux/amd64,linux/arm64 --push -t dgsspfdjw.org.cn:443/elasticsearch:8.10.4 
---
+++ hmplat/docker_build.sh
@@ -0,0 +1,3 @@
+# docker build -t coopersoft/elasticsearch:8.9.0 .
+
+docker buildx build . --platform linux/amd64 --push -t dgsspfdjw.org.cn:443/hmplat:1.0.0 
